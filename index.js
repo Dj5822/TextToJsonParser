b7305e4ded8368156b15b506d6cd1a1427a2baa9
dj5822 <Dj5822@gmail.com>
2021-11-29 00:13:18 +1300

    Character parsing is fully functional

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 import db from "./utils/firebase.js";
 import { collection, doc, setDoc } from "firebase/firestore";
+import fs from "fs";
 
-const charactersRef = doc(collection(db, "characters"));
+function readData(filePath, cb) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+        var splitData = data.replace(/(\r\n|\n|\r)/gm, "").split(/Title: /);
+        
+        var characterList = [];
+        splitData.forEach(c => {
+            var characterInfo = c.split(/Name: |Description: /);
+            characterList.push({
+                name: characterInfo[1],
+                title: characterInfo[0],
+                description: characterInfo[2]
+            });
+        }); 
+        cb(characterList);
+    });
+}
 
-await setDoc(charactersRef, {
-    id: charactersRef.id,
-    name: "Fenrir",
-    title: "",
-    description: ""
+readData('./data.txt', (data) => {
+    data.forEach(async (characterInfo) => {
+        const charactersRef = doc(collection(db, "characters"));
+
+        await setDoc(charactersRef, {
+            id: charactersRef.id,
+            name: String(characterInfo["name"]),
+            title: String(characterInfo["title"]),
+            description: String(characterInfo["description"])
+        }).catch(err => {
+            console.log(err);
+        }).then(() => {
+            console.log(characterInfo);
+            console.log("Document written with ID: ", charactersRef.id);
+        });        
+    });    
+
 });
-
-console.log("Document written with ID: ", charactersRef.id);
