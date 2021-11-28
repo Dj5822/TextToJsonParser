d23b4a4f185d9431c22b5c8686cac23af0b8de05
dj5822 <Dj5822@gmail.com>
2021-11-28 23:14:12 +1300

    Can add documents to the characters collection

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("test");
+import db from "./utils/firebase.js";
+import { collection, doc, setDoc } from "firebase/firestore";
+
+const charactersRef = doc(collection(db, "characters"));
+
+await setDoc(charactersRef, {
+    id: charactersRef.id,
+    name: "Fenrir",
+    title: "",
+    description: ""
+});
+
+console.log("Document written with ID: ", charactersRef.id);
