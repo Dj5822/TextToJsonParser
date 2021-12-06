8cf79cdfeb5b8343a8b1d90cd85a8b85ddf48f65
dj5822 <Dj5822@gmail.com>
2021-12-06 20:29:02 +1300

    id is now based on the character name

--- index.js
+++ index.js
@@ -6,38 +6,37 @@ function readData(filePath, cb) {
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            return
         }
         var splitData = data.replace(/(\r\n|\n|\r)/gm, "").split(/Title: /);
         
         var characterList = [];
         splitData.forEach(c => {
-            var characterInfo = c.split(/Name: |Description: /);
-            characterList.push({
-                name: characterInfo[1],
-                title: characterInfo[0],
-                description: characterInfo[2]
-            });
+            if (c != "") {
+                var characterInfo = c.split(/Name: |Description: /);
+                characterList.push({
+                    name: characterInfo[1],
+                    title: characterInfo[0],
+                    description: characterInfo[2]
+                });
+            }
         }); 
         cb(characterList);
     });
 }
 
 readData('./data.txt', (data) => {
-    data.forEach(async (characterInfo) => {
-        const charactersRef = doc(collection(db, "characters"));
-
-        await setDoc(charactersRef, {
-            id: charactersRef.id,
+    data.forEach(async (characterInfo) => {        
+        const id = characterInfo["name"].replace(" ", "_");
+        const charactersRef = setDoc(doc(db, "characters", id), {
+            id: id,
             name: String(characterInfo["name"]),
             title: String(characterInfo["title"]),
             description: String(characterInfo["description"])
         }).catch(err => {
             console.log(err);
         }).then(() => {
-            console.log(characterInfo);
-            console.log("Document written with ID: ", charactersRef.id);
-        });        
+            console.log("Document written with ID: ", id);
+        });    
     });    
 
 });
