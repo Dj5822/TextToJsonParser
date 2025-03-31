2123b178ef41c2ee59aa6da3364c94bd4b087927
Dylan Jung <Dj5822@gmail.com>
2025-03-31 19:56:37 +1300

    Use synchronous IO and functional programming for the script

--- index.ts
+++ index.ts
@@ -1,42 +1,39 @@
-// import fs from "fs";
+import fs from "fs";
 
-// function readData(filePath, cb) {
-//     fs.readFile(filePath, 'utf8', (err, data) => {
-//         if (err) {
-//             console.error(err);
-//         }
-//         var splitData = data.replace(/(\r\n|\n|\r)/gm, "").split(/Title: /);
+interface Character {
+  name: string;
+  title: string;
+  description: string;
+}
 
-//         var characterList = [];
-//         splitData.forEach(c => {
-//             if (c != "") {
-//                 var characterInfo = c.split(/Name: |Description: /);
-//                 characterList.push({
-//                     name: characterInfo[1],
-//                     title: characterInfo[0],
-//                     description: characterInfo[2]
-//                 });
-//             }
-//         });
-//         cb(characterList);
-//     });
-// }
+const readData = (filePath: string): Character[] => {
+  let data: string = "";
+  try {
+    data = fs.readFileSync(filePath, "utf8");
+  } catch (err) {
+    console.log(err);
+  }
 
-// readData('./data.txt', (data) => {
-//     data.forEach(async (characterInfo) => {
-//         const id = characterInfo["name"].replace(" ", "_");
-//         const charactersRef = setDoc(doc(db, "characters", id), {
-//             id: id,
-//             name: String(characterInfo["name"]),
-//             title: String(characterInfo["title"]),
-//             description: String(characterInfo["description"])
-//         }).catch(err => {
-//             console.log(err);
-//         }).then(() => {
-//             console.log("Document written with ID: ", id);
-//         });
-//     });
+  return data
+    .replace(/(\r\n|\n|\r)/gm, "")
+    .split(/Title: /)
+    .filter((c) => c != "")
+    .map((c) => {
+      const characterInfo = c.split(/Name: |Description: /);
+      return {
+        name: characterInfo[1],
+        title: characterInfo[0],
+        description: characterInfo[2],
+      };
+    });
+};
 
-// });
+function main() {
+  const charList: Character[] = readData("./data.txt");
 
-console.log("hello world");
+  console.log(charList);
+
+  fs.writeFileSync("./output.json", JSON.stringify(charList));
+}
+
+main();
